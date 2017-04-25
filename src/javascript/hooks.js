fbe2cb66169245d561b6b0d2b019b035379f3abe
Elad R. Katz <eladkt@gmail.com>
2017-04-26 00:36:53 +0300

    basic redux working. barely :-$

--- src/javascript/hooks.js
+++ src/javascript/hooks.js
@@ -1,14 +1,17 @@
 import { bindActionCreators } from 'redux';
 import * as UserActions from './redux/modules/users';
 import * as BlogActions from './redux/modules/blogposts';
+import * as ImageSearchActions from './redux/modules/imagesearches'
 
 export function bootstrap({ dispatch }) {
   const userActions = bindActionCreators(UserActions, dispatch);
   const blogActions = bindActionCreators(BlogActions, dispatch);
+  const imageSearchActions = bindActionCreators(ImageSearchActions, dispatch);
 
   return () => {
     blogActions.fetchPosts(0, 10);
     userActions.fetchUsers();
+    imageSearchActions.fetchImages();
   };
 }
 
--- src/javascript/redux/modules/imageSearches.js
+++ src/javascript/redux/modules/imageSearches.js
@@ -1,4 +1,6 @@
 const SEARCH_IMAGE = 'imagesearches/SEARCH_IMAGE';
+const SEARCH_IMAGE_SUCCESS = 'imagesearches/SEARCH_IMAGE_SUCCESS';
+const SEARCH_IMAGE_FAILURE = 'imagesearches/SEARCH_IMAGE_FAILURE';
 
 const initialState = [
   {imageName:"first image name", index :0},
@@ -10,19 +12,24 @@ export default function reducer(state = initialState, action = {}) {
   const { type, payload } = action;
 
   switch (type) {
+  case SEARCH_IMAGE_SUCCESS:
+    debugger;
+    return [...payload];
+
   default:
     return state;
   }
 }
 
-export function fetchPosts(start = 0, limit = 10) {
-  // return async (dispatch) => {
-  //   dispatch({
-  //     type: [FETCH_POSTS, FETCH_POSTS_SUCCESS, FETCH_POSTS_FAILURE],
-  //     meta: {
-  //       fetch: [`/post?_start=${start}&_limit=${limit}`, {method: 'get'}]
-  //     }
-  //   });
-  // };
-}
 
+export function fetchImages(start = 0, limit = 10) {
+  return async (dispatch) => {
+    debugger;
+    dispatch({
+      type: [SEARCH_IMAGE, SEARCH_IMAGE_SUCCESS, SEARCH_IMAGE_FAILURE],
+      meta: {
+        fetch: [`/post?_start=${start}&_limit=${limit}`, {method: 'get'}]
+      }
+    });
+  };
+}
